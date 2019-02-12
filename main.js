20bc1864700fcfbb0a32583afcd30dca939bccf2
Aidan McKay <aidanmckay2000@gmail.com>
2019-02-11 17:49:54 -0700

    Adding the template literal and corresponding global variables
    Co-authored-by: Jacqueline Moore <jacquelinebmoore@gmail.com>

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-var
+var titleInput = document.querySelector('#title-input').value;
+var bodyInput = document.querySelector('#body-input').value;
+var saveButton = document.querySelector('#save-btn');
+var ideaTitleInput = document.querySelector('#title-input').value;
+var ideaBodyInput = document.querySelector('#body-input').value;
+var ideaBox = document.querySelector('.ideas');
+
+
+
+
+saveButton.addEventListener('click', function(){
+  displayIdea(ideaTitleInput, ideaBodyInput);
+})
+
+function displayIdea (ideaTitle, ideaBody) {
+  var ideaBoxes =`<section class="idea-box">
+                  <h3 class="idea-box-title">${ideaTitle}</h3>
+                  <h4 class="idea-box-body">${ideaBody}</h4>
+                  <hr class="idea-box-seperation">`
+ideaBox.insertadjacentHTML('beforeend', ideaBoxes);
+}
