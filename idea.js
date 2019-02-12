4823f8fe5f0aa696777d66bb75c358e1c2270a25
Aidan McKay <aidanmckay2000@gmail.com>
2019-02-12 12:48:37 -0700

    Adding the HTML for the card in HTML document

---
+++ idea.js
@@ -0,0 +1,15 @@
+class ideaCards {
+  constructor(title, body, cardId) {
+    this.title = title;
+    this.body = body;
+    this.quality = 'Swill';
+    this.cardId = cardId;
+  }
+}
+//function updateContent
+
+//function deleteFromStorage
+
+//function saveToStorage
+
+//function updateQuality
--- main.js
+++ main.js
@@ -5,20 +5,21 @@ var ideaTitleInput = document.querySelector('#title-input').value;
 var ideaBodyInput = document.querySelector('#body-input').value;
 var ideaBox = document.querySelector('.ideas');
 // var quality = document.querySelector('')
-
+// var card areas
+// var cardId = 1 (cardID++)
 
 
 
 saveButton.addEventListener('click', function(){
-  displayIdea(ideaTitleInput, ideaBodyInput);
+  displayIdea(cardId, ideaTitleInput, ideaBodyInput);
 })
 
-function displayIdea (ideaTitle, ideaBody, quality) {
-  var ideaBoxes =`<section class="idea-box">
-                  <h3 class="idea-box-title">${ideaTitle}</h3>
-                  <h4 class="idea-box-body">${ideaBody}</h4>
-                  <hr class="idea-box-seperation">
-                  <h5 class="quality">Quality: ${quality}</h5>
-                  </section>`
-ideaBox.insertadjacentHTML('beforeend', ideaBoxes);
-}
+// function displayIdea (cardId, ideaTitle, ideaBody, quality) {
+//   var html =      `<section class="idea-box ideaBoxId${cardId}">
+//                   <h3 class="idea-box-title">${ideaTitle}</h3>
+//                   <h4 class="idea-box-body">${ideaBody}</h4>
+//                   <hr class="idea-box-seperation">
+//                   <h5 class="quality">Quality: ${quality}</h5>
+//                   </section>`
+// ideaBox.insertAdjacentHTML('beforeend', html);
+// }
