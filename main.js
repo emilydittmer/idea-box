1b046e882b16ac110cae933fd6ba40e234b3b139
Aidan McKay <aidanmckay2000@gmail.com>
2019-02-11 18:09:58 -0700

    Adding the quality to the template literal with no global variable to match

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var saveButton = document.querySelector('#save-btn');
 var ideaTitleInput = document.querySelector('#title-input').value;
 var ideaBodyInput = document.querySelector('#body-input').value;
 var ideaBox = document.querySelector('.ideas');
-
+// var quality = document.querySelector('')
 
 
 
@@ -12,10 +12,12 @@ saveButton.addEventListener('click', function(){
   displayIdea(ideaTitleInput, ideaBodyInput);
 })
 
-function displayIdea (ideaTitle, ideaBody) {
+function displayIdea (ideaTitle, ideaBody, quality) {
   var ideaBoxes =`<section class="idea-box">
                   <h3 class="idea-box-title">${ideaTitle}</h3>
                   <h4 class="idea-box-body">${ideaBody}</h4>
-                  <hr class="idea-box-seperation">`
+                  <hr class="idea-box-seperation">
+                  <h5 class="quality">Quality: ${quality}</h5>
+                  </section>`
 ideaBox.insertadjacentHTML('beforeend', ideaBoxes);
 }
