51ee7490ec2ccb4de2a73899cb93c350452cfec2
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-02-20 11:51:07 -0700

    Added data attribute
    Co-authored by: Emily Dittmer <emilyedittmer@gmail.com>
    Co-authored by: Aidan McKay <aidanmckay2000@gmail.com>

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Global Variables//
+//Global Variablaes//
 var titleInput = document.querySelector('#title-input');
 var bodyInput = document.querySelector('#body-input');
 var saveBtn = document.querySelector('#save-btn');
